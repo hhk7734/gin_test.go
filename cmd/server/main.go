4a2c53fd34e815f72eb31d899907dce695a8f2fb
Hyeonki Hong <hhk7734@gmail.com>
2023-03-30 20:04:19 +0900

    chore(swagger): remove docs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,18 +6,11 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/hhk7734/git-test/internal/config"
-	"github.com/hhk7734/git-test/internal/user_interface/gin/middleware"
+	"github.com/hhk7734/gin-test/internal/config"
+	"github.com/hhk7734/gin-test/internal/user_interface/gin/middleware"
 	"go.uber.org/zap"
 )
 
-// @title       Gin test
-// @version     1.0
-// @description Gin test
-
-// @schemes  http
-// @host     localhost:8080
-// @BasePath /
 func main() {
 	config.Init()
 	c := config.Config()
