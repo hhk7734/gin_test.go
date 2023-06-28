c5ad17af2e749e8d0913f05e55c09cd678a5a927
Hyeonki Hong <hhk7734@gmail.com>
2023-06-29 00:57:49 +0900

    feat(main): implement graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hhk7734/gin-test/internal/pkg/logger"
 	"github.com/hhk7734/gin-test/internal/user_interface/restapi/middleware"
 	"go.uber.org/zap"
 )
@@ -24,19 +29,52 @@ func main() {
 		workDir = filepath.Dir(workDir)
 	}
 
-	r := gin.New()
-	r.Use(middleware.LoggerWithZap([]string{}))
-	r.Use(middleware.RecoveryWithZap())
+	engin := gin.New()
+	engin.Use(middleware.LoggerWithZap([]string{}))
+	engin.Use(middleware.RecoveryWithZap())
 
-	r.StaticFile("/openapi.yaml", "web/static/openapi.yaml")
+	engin.StaticFile("/openapi.yaml", "web/static/openapi.yaml")
 
-	s := &http.Server{
+	server := &http.Server{
 		Addr:         ":8080",
-		Handler:      r,
+		Handler:      engin,
 		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
-	if err := s.ListenAndServe(); err != nil {
-		zap.L().Panic("Server error", zap.Error(err))
+
+	listenErr := make(chan error, 1)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			listenErr <- err
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err := <-listenErr:
+		logger.Logger(context.Background()).Error("failed to listen and serve", zap.Error(err))
+	case <-shutdown:
 	}
+
+	logger.Logger(context.Background()).Info("shutting down server...")
+
+	wg := sync.WaitGroup{}
+
+	go func() {
+		defer wg.Done()
+
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+
+		// blocked until all connections are closed or timeout
+		if err := server.Shutdown(ctx); err != nil {
+			logger.Logger(context.Background()).Error("failed to shutdown server", zap.Error(err))
+		}
+	}()
+	wg.Add(1)
+
+	wg.Wait()
 }
