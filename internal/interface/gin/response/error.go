3ae73f3df2e0d908d599f16d82ce4e06da38500b
Hyeonki Hong <hhk7734@gmail.com>
2023-06-30 03:01:39 +0900

    feat(interface): ErrorResponse 추가

---
+++ internal/interface/gin/response/error.go
@@ -0,0 +1,31 @@
+package response
+
+type RequestError struct {
+	Code    int    `json:"code"`
+	Message string `json:"msg"`
+}
+
+type ErrorResponse struct {
+	Error RequestError `json:"error"`
+}
+
+func (r ErrorResponse) Message(m string) ErrorResponse {
+	return ErrorResponse{Error: RequestError{Code: r.Error.Code, Message: m}}
+}
+
+func (r ErrorResponse) Code(c int) ErrorResponse {
+	return ErrorResponse{Error: RequestError{Code: c, Message: r.Error.Message}}
+}
+
+// //
+// http.StatusUnprocessableEntity
+// //
+const (
+	_ int = iota
+	RequestIDRequired
+)
+
+var (
+	// X-Request-Id 헤더가 없을 때 반환되는 에러
+	RequestIDRequiredResponse = ErrorResponse{Error: RequestError{Code: RequestIDRequired, Message: "X-Request-Id header is required"}}
+)
