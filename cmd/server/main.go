fa3484b4cec4c90b5befbc4a96bd86a28d9e0253
Hyeonki Hong <hhk7734@gmail.com>
2023-06-29 22:34:24 +0900

    refactor(logger): move logger middleware to internal/pkg

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/hhk7734/gin-test/internal/pkg/logger"
-	"github.com/hhk7734/gin-test/internal/user_interface/restapi/middleware"
 	"go.uber.org/zap"
 )
 
@@ -29,9 +28,11 @@ func main() {
 		workDir = filepath.Dir(workDir)
 	}
 
+	lm := logger.GinLoggerMiddleware{}
+
 	engin := gin.New()
-	engin.Use(middleware.LoggerWithZap([]string{}))
-	engin.Use(middleware.RecoveryWithZap())
+	engin.Use(lm.Logger)
+	engin.Use(lm.Recovery)
 
 	engin.StaticFile("/openapi.yaml", "web/static/openapi.yaml")
 
