035a7eb8c8e2f8d069cceb3084dcde39e45182bd
Hyeonki Hong <hhk7734@gmail.com>
2024-07-21 17:22:09 +0900

    feat(logger): add log_format option

--- internal/pkg/logger/logger.go
+++ internal/pkg/logger/logger.go
@@ -7,37 +7,51 @@ import (
 )
 
 const (
-	LOG_LEVEL_KEY = "log_level"
+	LOG_LEVEL_KEY  = "log_level"
+	LOG_FORMAT_KEY = "log_format"
 )
 
 type LogConfig struct {
-	Level string
+	Level  string
+	Format string
 }
 
 func LogPFlags() *pflag.FlagSet {
 	f := pflag.NewFlagSet("log", pflag.ContinueOnError)
 	f.String(LOG_LEVEL_KEY, "info", "log level")
+	f.String(LOG_FORMAT_KEY, "json", "log format")
 	return f
 }
 
 func LogConfigFromViper() LogConfig {
 	return LogConfig{
-		Level: viper.GetString(LOG_LEVEL_KEY)}
+		Level:  viper.GetString(LOG_LEVEL_KEY),
+		Format: viper.GetString(LOG_FORMAT_KEY),
+	}
 }
 
 func SetGlobalZapLogger(cfg LogConfig) {
 	var l *zap.Logger
-	zapCfg := zap.NewProductionConfig()
+	var zapCfg zap.Config
+
+	if cfg.Format != "json" {
+		zapCfg = zap.NewDevelopmentConfig()
+	} else {
+		zapCfg = zap.NewProductionConfig()
+		zapCfg.EncoderConfig.TimeKey = "time"
+	}
+
 	err := zapCfg.Level.UnmarshalText([]byte(cfg.Level))
 	if err != nil {
 		panic(err)
 	}
-	zapCfg.EncoderConfig.TimeKey = "time"
+
 	l, _ = zapCfg.Build()
 	defer l.Sync()
 	zap.ReplaceGlobals(l)
 
 	zap.L().Info("logger config", zap.Dict("config",
 		zap.String(LOG_LEVEL_KEY, cfg.Level),
+		zap.String(LOG_FORMAT_KEY, cfg.Format),
 	))
 }
